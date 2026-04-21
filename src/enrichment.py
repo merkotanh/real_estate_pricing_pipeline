51c9b1e3731853c56affdd77a8f324fa7d0e43ea
merkotanh <gala.mrt@gmail.com>
2026-04-21 09:45:32 +0200

    add initial data samples

--- src/enrichment.py
+++ src/enrichment.py
@@ -1,9 +1,5 @@
 import pandas as pd
 
-#dfb = pd.merge(df_cities, df_land, left_on = 'bundesland', right_on = 'long_name', how = 'left')  # df_cities =postleitzahlen.csv"
-#dfm = pd.merge(dfb, df_population, left_on = 'plz', right_on = 'plz', how = 'left')                #df_land = bundesland.xlsx"
-#df_immo_data_plz_city = pd.merge(df, dfm, left_on = ['post', 'city'], right_on = ['plz', 'ort'], how = 'left')  #df_population = plz_gebiete.xlsx
-
 #    df_population -> plz_gebiete.xlsx  ['plz', 'note', 'population', 'area_qkm']
 #    df_bundesland -> bundesland.xlsx ['short_name', 'long_name', 'population_land', 'cadaster_area']
 #    df_cities -> postleitzahlen.csv  plz
